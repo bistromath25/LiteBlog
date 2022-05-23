b9c677881e4a3ab429326a3bd482fecad9abd296
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2022-05-23 14:32:21 +0000

    Complete re-write, see the "old" branch for the original stuff

--- template/loadtemplate.js
+++ template/loadtemplate.js
@@ -1,9 +1,7 @@
 $(function(){
     try {
         $("#header").load("template/header.html");
-        $("#sidebar-backbutton").load("template/sidebar-backbutton.html");
-        $("#sidebar-sitemap").load("template/sidebar-sitemap.html");
-        $("#footer").load("template/footer.html");
+        $("#sidebar").load("template/sidebar.html");        
         console.log("All templates loaded successfully");
     } catch(e) {
         console.log("Some templates not loaded successfully")
