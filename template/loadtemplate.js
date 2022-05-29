5e6382c0d16e8da5ac97c787a59c11c0aec17a32
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2022-05-29 16:22:08 +0000

    Link highlight and border-radius

--- template/loadtemplate.js
+++ template/loadtemplate.js
@@ -1,7 +1,8 @@
 $(function(){
     try {
         $("#header").load("template/header.html");
-        $("#sidebar").load("template/sidebar.html");        
+        $("#sidebar").load("template/sidebar.html");   
+        $("#topbutton").load("template/topbutton.html");
         console.log("All templates loaded successfully");
     } catch(e) {
         console.log("Some templates not loaded successfully")
